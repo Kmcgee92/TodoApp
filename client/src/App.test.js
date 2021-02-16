20f473bb77af43465e86b896a237d2bfc82e130b
kmcgee92 <kaseycmcgee@gmail.com>
2021-02-15 19:52:45 -0700

    install testing dev-deps

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true
+})
+
+
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
